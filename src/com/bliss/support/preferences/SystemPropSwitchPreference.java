Havoc-OS/android_vendor_support

support: Add SystemPropSwitchPreference
Change-Id: I25a6a77ae6631d53edb65c9c0b24c41b3e398fc8
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/havoc/support/preferences/SystemPropSwitchPreference.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2020 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SystemPropSwitchPreference extends SwitchPreference {
+
+    public SystemPropSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    public SystemPropSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    public SystemPropSwitchPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default TwoStatePreference implementation is doing without respecting
+        // real default value:
+        //setChecked(restoreValue ? getPersistedBoolean(mChecked)
+        //        : (Boolean) defaultValue);
+        // Instead, we better do
+        setChecked(restoreValue ? getPersistedBoolean((Boolean) defaultValue)
+                : (Boolean) defaultValue);
+    }
+}

Night-Raids-Reborn/bliss_vendor_support

src/com/bliss/support/preferences/SystemPropSwitchPreference.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2020 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bliss.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SystemPropSwitchPreference extends SwitchPreference {
+
+    public SystemPropSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    public SystemPropSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    public SystemPropSwitchPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default TwoStatePreference implementation is doing without respecting
+        // real default value:
+        //setChecked(restoreValue ? getPersistedBoolean(mChecked)
+        //        : (Boolean) defaultValue);
+        // Instead, we better do
+        setChecked(restoreValue ? getPersistedBoolean((Boolean) defaultValue)
+                : (Boolean) defaultValue);
+    }
+}